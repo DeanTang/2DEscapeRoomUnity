3e9474ccc276625dfe5ffbe827facf19e1e30461
DeanTang <30429736+DeanTang@users.noreply.github.com>
2024-10-14 17:14:06 +1100

    Added Zoom In Object interface and box

---
+++ 2DEscapeRoom/Assets/Scripts/IInteractable.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public interface IInteractable 
+{
+    void Interact(DisplayImage currentDisplay);
+}
---
+++ 2DEscapeRoom/Assets/Scripts/ZoomInObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class ZoomInObject : MonoBehaviour, IInteractable
+{
+    public float ZoomRatio = .5f;
+    public void Interact(DisplayImage currentDisplay)
+    {
+        Camera.main.orthographicSize *= ZoomRatio;
+        Camera.main.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Camera.main.transform.position.z);
+    }
+}
